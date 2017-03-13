dde9b21e7fa488cf90da55bca5a7572b1a29ec47
tgrosh <tgrosh@yahoo.com>
2017-03-12 23:04:18 -0400

    Adjusted Grass Color for Grass Rocks

--- Assets/Scripts/NoFlyZone.cs
+++ Assets/Scripts/NoFlyZone.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NoFlyZone : MonoBehaviour {
-    public float forceScale;
-
 	// Use this for initialization
 	void Start () {
 		
