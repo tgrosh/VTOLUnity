3b63cd42e27597fb5a6821ce1376d6ea88502cd5
tgrosh <tgrosh@yahoo.com>
2017-03-16 22:00:54 -0400

    Trigger is Triggerable

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trigger : MonoBehaviour {
+public class Trigger : Triggereble {
     public Triggereble target;
     public bool transportTriggerOnly = true;
 
     GameObject lastColliderRoot;
     List<Collider> colliders = new List<Collider>();
+    bool triggerIsActive = true;
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +23,8 @@ public class Trigger : MonoBehaviour {
 
     void OnTriggerEnter(Collider collider)
     {
+        if (!triggerIsActive) return;
+
         GameObject colliderRoot = collider.transform.root.gameObject;
 
         if (transportTriggerOnly && colliderRoot.GetComponent<Transport>() == null)
@@ -38,10 +42,17 @@ public class Trigger : MonoBehaviour {
     
     void OnTriggerExit(Collider collider)
     {
+        if (!triggerIsActive) return;
+
         colliders.Remove(collider);
         if (colliders.Count == 0)
         {
             lastColliderRoot = null;
         }
+    }
+
+    public override void OnTrigger(Trigger trigger)
+    {
+        triggerIsActive = !triggerIsActive;
     }
 }
