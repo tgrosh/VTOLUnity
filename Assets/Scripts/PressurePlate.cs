381009fb322accb310dfc074980ae49fae973d54
tgrosh <tgrosh@yahoo.com>
2018-03-20 22:08:56 -0400

    New Home Pad and Pressure Plate

--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -3,19 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PressurePlate : MonoBehaviour {
+    public float triggerWeight;
+    public Triggerable target;
+    public float debounceThreashold;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    bool triggered;
+    float lastCollisionTime;
+    int collisionCount;
+
+    void Reset () {
+        triggerWeight = 75f;
+        debounceThreashold = 1f;
 	}
 
     void OnCollisionEnter (Collision collision)
     {
-        Debug.Log("meep");
+        collisionCount++;
+        Debug.Log("Collision Detected (" + collisionCount + ") :" + (Time.realtimeSinceStartup - lastCollisionTime));
+        if (!triggered && Time.realtimeSinceStartup - lastCollisionTime > debounceThreashold)
+        {
+            Debug.Log("Not Triggered");
+            if (collision.collider.transform.root.GetComponent<Rigidbody>().mass > triggerWeight)
+            {
+                Debug.Log("Triggering");
+                triggered = true;
+                target.OnTrigger(null);
+            }
+        }
+        lastCollisionTime = Time.realtimeSinceStartup;
+    }
+    void OnCollisionExit()
+    {
+        collisionCount--;
+        triggered = collisionCount != 0;
+        Debug.Log("Collision Left (" + collisionCount + ")");
     }
 }
