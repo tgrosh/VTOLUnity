c832852413a826f39ef473d57227634e287f6843
tgrosh <tgrosh@yahoo.com>
2018-03-28 12:20:24 -0400

    Transport Auto Hover

--- Assets/Scripts/Thruster.cs
+++ Assets/Scripts/Thruster.cs
@@ -17,6 +17,12 @@ public class Thruster : MonoBehaviour {
     public float thrusterCutoutDuration = .25f;
     float thrusterCutoutChance = .5f;
 
+    public bool autoHover;
+    public float autoHoverThrust = .001f;
+    public float currentAutoHoverThrust = 0f;
+    public float hoverHeight = 0f;
+    public float targetVelocity = 0f;
+
     bool cutout;
     float thrusterCutoutCurrent = 0f;
     ParticleSystem.MainModule engineMain;
@@ -76,6 +82,48 @@ public class Thruster : MonoBehaviour {
             thrusterCutoutCurrent = 0f;
         }
 
+        if (autoHover)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit))
+            {
+                // 0..1 percentage of desired height remaining. 0 = at height
+                float hoverHeightPercent = Mathf.Abs((hoverHeight - hit.distance) / hoverHeight);
+
+                //desired velocity with respect to how far we are from desired height. 
+                // farther from desired height means more velocity desired
+                float currentTargetVelocity = Mathf.Lerp(0, targetVelocity, hoverHeightPercent);
+
+                // 0..1 percentage of desired velocity remaining. 0 = at desired velocty
+                float velocityPercent = Mathf.Abs((currentTargetVelocity - forceBody.velocity.y) / currentTargetVelocity);
+
+                if (hit.distance <= hoverHeight) //need to go up
+                {
+                    if (forceBody.velocity.y < currentTargetVelocity) //at or below target velocity
+                    {
+                        //need more height, but not at target velocity, so add more thrust
+                        currentAutoHoverThrust += Mathf.SmoothStep(0, autoHoverThrust, velocityPercent);
+                    } else //rising faster than target velocity
+                    {
+                        currentAutoHoverThrust -= autoHoverThrust * (1-hoverHeightPercent);
+                    }
+                } else //need to go down
+                {
+                    if (forceBody.velocity.y < -currentTargetVelocity) //falling too fast
+                    {
+                        //need more height, but not at target velocity, so add more thrust
+                        currentAutoHoverThrust += Mathf.SmoothStep(0, autoHoverThrust, velocityPercent);
+                    } else //not falling fast enough
+                    {
+                        currentAutoHoverThrust -= autoHoverThrust * (velocityPercent);
+                    }
+                }
+            }
+            thrustValue = currentAutoHoverThrust;
+        } else
+        {
+            currentAutoHoverThrust = 0f;
+        }
 
         if (cutout || fuelTank.fuelRemaining <= 0)
         {
@@ -107,4 +155,5 @@ public class Thruster : MonoBehaviour {
             cutoff.Play();
         }
     }
+        
 }
