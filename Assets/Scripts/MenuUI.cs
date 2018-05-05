715b0a625b15cd86ccf61a98ebdad04699d21599
tgrosh <tgrosh@yahoo.com>
2018-05-04 23:51:38 -0400

    Start Menu

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUI : MonoBehaviour {
+    public Cinemachine.CinemachineVirtualCamera MenuCam;
+    public GameObject InputController;
+
+    void Start()
+    {
+        InputController.SetActive(false);
+    }
+
+    void Update()
+    {
+        //look for Start Button
+        if (Input.GetButtonDown("Start"))
+        {
+            OnStartPressed();
+        }
+    }
+
+    public void OnStartPressed()
+    {
+        MenuCam.gameObject.SetActive(false);
+        InputController.SetActive(true);
+        Invoke("Hide", 1f);
+    }
+
+    public void Hide()
+    {
+        GetComponent<CanvasGroup>().alpha = 0;
+    }
+}
