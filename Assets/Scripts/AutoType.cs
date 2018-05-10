5964a3f7c91a9f4d5e2d46894c0137bedadc55f8
tgrosh <tgrosh@yahoo.com>
2018-05-09 22:39:07 -0400

    Conversation Panel (WIP)

---
+++ Assets/Scripts/AutoType.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoType : MonoBehaviour {
+    public AudioSource audioSource;
+    public float letterDelay = 0.2f;
+    public AudioClip sound;
+
+    string message;
+    Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+        message = text.text;
+        text.text = "";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in message.ToCharArray())
+        {
+            text.text += letter;
+            if (audioSource && sound)
+                audioSource.PlayOneShot(sound);
+            yield return 0;
+            yield return new WaitForSeconds(letterDelay);
+        }
+    }
+}
