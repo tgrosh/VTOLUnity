18f92570b89ead78f992365e9ed46df1d51fdbd5
tgrosh <tgrosh@yahoo.com>
2021-02-04 15:57:47 -0500

    Merge from master

---
+++ Assets/Scripts/DialogTrack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace Assets.Scripts
+{
+    [TrackBindingType(typeof(Dialog))]
+    [TrackClipType(typeof(DialogClip))]
+    public class DialogTrack : TrackAsset
+    {
+        public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
+        {
+            return ScriptPlayable<DialogTrackMixer>.Create(graph, inputCount);
+        }
+    }
+}
